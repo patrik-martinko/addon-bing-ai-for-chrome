4e34b89538c4e94c17631b4a331fafc43dd85f90
vladkryv <83036866+vladkryv@users.noreply.github.com>
2024-01-24 21:48:26 +0200

    Set additional header

--- package/background.js
+++ package/background.js
@@ -11,6 +11,16 @@ chrome.declarativeNetRequest.updateDynamicRules({
                         operation: 'set',
                         header: 'user-agent',
                         value: navigator.userAgent.split('AppleWebKit')[0] + 'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0'
+                    },
+                    {
+                        operation: 'set',
+                        header: 'sec-ch-ua',
+                        value: '\"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\", \"Microsoft Edge\";v=\"120\"'
+                    },
+                    {
+                        operation: 'set',
+                        header: 'sec-ch-ua-full-version-list',
+                        value: '\"Not_A Brand\";v=\"8.0.0.0\", \"Chromium\";v=\"120.0.6099.234\", \"Microsoft Edge\";v=\"120.0.2210.144\"'
                     }
                 ]
             },
